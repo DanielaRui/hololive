5c2c6d2295b8b070f04ec528bc4bfd4010844e38
danielarui <aleinadanirakzuirzednanreh@gmail.com>
2023-10-24 00:02:26 -0600

    first commit

---
+++ Express/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json())
+
+//importante
+app.use(
+    cors({
+      origin: "http://localhost:8100",
+    })
+);
+
+app.get('/', function(req, res) {
+    res.send({nombre:"Gawr Gura"});
+});
+
+app.post('/', function(req, res) {
+    res.send(req.body);
+});
+
+const server = app.listen(4000, () => {
+    console.log("Servidor corriendo en el puerto 4000");
+});
+
